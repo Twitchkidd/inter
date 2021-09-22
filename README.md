e8d5b1953121400777f37b0c2ad14f32a70df3d8
Gareth Field <field.gareth@gmail.com>
2021-09-22 10:08:48 -0400

    works

--- README.md
+++ README.md
@@ -9,3 +9,5 @@
 Eugh, sort is `a,b => a - /\* MINUS \*/ b not a,b => a < /\* LESS THAN \*/ b`. I didn't need to make the code that small, either. Nice, but took too long (like 40 minutes,) you can go faster.
 
 ### secondNum
+
+Okay, 15 minutes is better, still need to go faster. Use new Set(array) and \[...set\] to get back to an array.
--- secondNum.js
+++ secondNum.js
@@ -9,3 +9,33 @@ const arr2 = [8, 23, 29, 25, 40, 0, 24];
 // sort
 // set
 // return
+
+const getSecondLargestAndSmallest = (first, second) => {
+	const combined = [...first, ...second];
+	const setted = new Set(combined);
+	if (setted.size < 2) {
+		return `
+    Either one or zero unique numbers, doesn't make sense!
+    `;
+	} else {
+		const arrayed = [...setted];
+		const sorted = arrayed.sort((a, b) => b - a);
+		return `
+    2nd Largest : ${sorted[1]}
+    
+    2nd Smalledt : ${sorted[sorted.length - 2]}
+    `;
+	}
+};
+
+const init = () => {
+	console.log(`
+  Welcome to the 2nd largest, 2nd smallest program!
+  `);
+	console.log(getSecondLargestAndSmallest(arr1, arr2));
+	console.log(`
+  Thank you for running the 2nd largest, 2nd smallest program!
+  `);
+};
+
+init();
