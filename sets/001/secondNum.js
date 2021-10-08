a2ac7e1456b91c99406911673754cacb196e3168
Gareth Field <field.gareth@gmail.com>
2021-10-08 11:54:42 -0400

    start review!

--- sets/001/secondNum.js
+++ sets/001/secondNum.js
@@ -1,5 +1,5 @@
-// *Find the 2nd largest and 2nd smallest number in two arrays of numbers combined
-// I.e. - [10,5,7,2,4,1,24] & [8,23,29,25,40,0,24] -> 2nd Largest : 29 , 2nd Smallest: 1 ----------**
+// * Find the 2nd largest and 2nd smallest number in two arrays of numbers combined
+// * I.e. - [10,5,7,2,4,1,24] & [8,23,29,25,40,0,24] -> 2nd Largest : 29 , 2nd Smallest: 1 ----------**
 
 const arr1 = [10, 5, 7, 2, 4, 1, 24];
 const arr2 = [8, 23, 29, 25, 40, 0, 24];
