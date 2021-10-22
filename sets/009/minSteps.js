8e9eff5dd0ff06a32db4d876dbb35b9aa09bb493
Gareth Field <field.gareth@gmail.com>
2021-10-22 08:22:13 -0400

    Saving!

--- sets/009/minSteps.js
+++ sets/009/minSteps.js
@@ -2,6 +2,11 @@
 
 // (... write a function to ...)
 
+const x = 10;
+const divs = [2, 3];
+
+const quotients = (n, ds) => ds.filter(d => n % d === 0).map(d => n / d);
+
 const minSteps = n => {
 	if (n > 500) {
 		return 'Got all day, buddy?';
@@ -27,9 +32,64 @@ const minSteps = n => {
 	return traverse(n);
 };
 
-const x = 501;
-
 console.time(`MINSTEPS ${x}:`);
 console.log('\x1b[35m%s\x1b[0m', `Calculating minSteps(${x}) ...`);
-console.log('\x1b[36m%s\x1b[0m', minSteps(x));
+console.log('\x1b[36m%s\x1b[0m', `Steps: ${minSteps(x)}`);
 console.timeEnd(`MINSTEPS ${x}:`);
+
+/*
+
+* So now the trick is to remember memoization and tabularization,
+  * and to turn this into something that I would write.
+
+*/
+
+// almost, take a step back and try divisors first
+// const ops = [n => n - 1, n => n / 3, n => n / 2];
+
+const mn = (n, ds) => {
+	const traverse = cur =>
+		cur === 1
+			? 0
+			: Math.min(traverse(cur - 1), ...quotients(cur, ds).map(traverse)) + 1;
+	return traverse(n);
+};
+
+// const mn = (n, ds) => {
+// 	const traverse = cur => {
+// 		if (cur === 1) return 0;
+// 		// if the operation is possible, check against the reductive case
+// 		const red = x => x - 1;
+// 		const cases = ds.filter(d => cur % d === 0).map(d => cur / d);
+//		// return Math.min(traverse(red(cur)), ...cases) + 1;
+//		return Math.min(traverse(red(cur)), ...cases.map(traverse)) + 1;
+// 	};
+// 	return traverse(n);
+// };
+
+console.time(`MN ${x}, DIV ${divs}:`);
+console.log('\x1b[35m%s\x1b[0m', `Calculating mn(${x}, divisors: ${divs}) ...`);
+console.log('\x1b[36m%s\x1b[0m', `Steps: ${mn(x, divs)}`);
+console.timeEnd(`MN ${x}, DIV ${divs}:`);
+
+const minStepsMemo = (n, ds) => {
+	let cache = {};
+	const traverse = cur => {
+		if (cur === 1) {
+			return 0;
+		}
+		if (cur in cache) {
+			return cache[cur];
+		}
+		const res =
+			Math.min(traverse(cur - 1), ...quotients(cur, ds).map(traverse)) + 1;
+		cache[cur] = res;
+		return res;
+	};
+	return traverse(n);
+};
+
+console.time(`MIN MEMO ${x}, DIV ${divs}:`);
+console.log('\x1b[35m%s\x1b[0m', `Calculating mn(${x}, divisors: ${divs}) ...`);
+console.log('\x1b[36m%s\x1b[0m', `Steps: ${mn(x, divs)}`);
+console.timeEnd(`MIN MEMO ${x}, DIV ${divs}:`);
